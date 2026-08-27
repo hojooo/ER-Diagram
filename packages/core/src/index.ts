4761c425917020920942e448683579e227fb0f9a
hojooo <ghwn5833@gmail.com>
2026-08-27 14:48:30 +0900

    feat(core): add project revision application use cases

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,7 @@
 export const corePackage = "@er-diagram/core";
 
+export * from "./application/project.js";
+export * from "./application/project-application.js";
 export * from "./dbml-parser.js";
 export * from "./hash.js";
 export * from "./schema-graph.js";
