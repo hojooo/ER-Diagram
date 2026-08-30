4a84f51b91c72b47f81fe533266c6e998d043b98
hojooo <ghwn5833@gmail.com>
2026-08-30 10:35:36 +0900

    feat(source-transform): route verified visual command patches

--- packages/source-transform/src/index.ts
+++ packages/source-transform/src/index.ts
@@ -1,7 +1,9 @@
 export const sourceTransformPackage = "@er-diagram/source-transform";
 
 export * from "./create-column.js";
+export * from "./groups-views.js";
 export * from "./relationships-indexes.js";
 export * from "./table-column.js";
 export * from "./text-edits.js";
 export * from "./types.js";
+export * from "./visual-command.js";
